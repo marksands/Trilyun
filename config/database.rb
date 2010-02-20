1eea46a2f7d446f9a45d8d11888580a26bffea13
marksands <marksands07@gmail.com>
2010-02-19 20:20:29 -0600

    moved logic into model...LIKE A BOSS

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,8 @@
 require 'sinatra/sequel'
+require 'open-uri'
+require 'nokogiri'
+require 'fastercsv'
+require 'nokogiri'
 require 'yaml'
 
 content = File.new("config/database.yml").read
@@ -7,13 +11,26 @@ settings = YAML::load(content)
 # database shiz
 set :database, "#{settings['adapter']}://#{Dir.pwd}/config/#{settings['database']}"
 
-migration "create csv table" do
+migration "create tyra_banks table" do
   database.create_table :tyra_banks do
     primary_key :id
     string      :filename
-    string      :path
   end
 end
 
-# as in model, get it? eh, eh??
-class TyraBanks < Sequel::Model; end
+# as in model, get it? eh, eh?? America's next top... forget it.
+class TyraBanks < Sequel::Model
+  def generate_file query, post
+    query = query[:query]
+    filename = post[:filename] + '.csv'
+    doc = Nokogiri::HTML( open(URI.escape( "http://www.google.com/search?num=100&q=#{query}")))
+    # write to file
+    FasterCSV.open( File.join( Dir.pwd, '/public/files', filename ), 'w' ) do |file|
+      file << ["result_num", "title", "link", "description"]
+      doc.xpath("//h3/a[@class='l']").each_with_index do |link, index|
+        description = doc.xpath("//div[@class='s']")[index]
+        file << [index+1, link.content, link['href'], description]
+      end
+    end  
+  end
+end
--- trilyun.rb
+++ trilyun.rb
@@ -1,8 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'open-uri'
-require 'nokogiri'
-require 'fastercsv'
 require 'config/database'
 
 before do
@@ -11,8 +8,7 @@ end
 
 helpers do
   def link_to url, title='Download'
-    base = request.script_name
-    "<a href='#{base}#{url}'>#{title}</a>"
+    "<a href='#{url}'>#{title}</a>"
   end
 end
 
@@ -22,30 +18,16 @@ get '/' do
 end
 
 post '/create' do
-  # create a new CSV
   @csv = TyraBanks.new(params[:tyrabanks])
+  @csv.generate_file(params[:post], params[:tyrabanks])
   if @csv.save
-    # parse the search
-    @@query = params[:post][:query]
-    @@filename = params[:tyrabanks][:filename] + '.csv'
-    @@url = URI.escape( "http://www.google.com/search?num=100&q=#{@@query}")
-    @@doc = Nokogiri::HTML( open(@@url) )
-    FasterCSV.open( File.join( Dir.pwd, '/public/files', @@filename ), 'w' ) do |file|
-      file << ["result_num", "title", "link", "description"]
-      @@doc.xpath("//h3/a[@class='l']").each_with_index do |link, index|
-        description = @@doc.xpath("//div[@class='s']")[index]
-        file << [index+1, link.content, link['href'], description]
-      end
-    end  
-    @csv.filename = @@filename
-    @csv.path = 'http://localhost:9393/files/' + @csv.filename
+    redirect('/download') 
+  else
+    redirect('/')
   end
-  @csv.save
-  redirect('/')
 end
 
 get '/download' do
   @csvs = database[:tyra_banks].all
   erb :download
 end
-
